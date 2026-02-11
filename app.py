84dd53afe8dcb6f4d77465a9a409158416176749
Marine <marinedoineau@gmail.com>
2026-02-11 21:01:42 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request
+import json
+
+app = Flask(__name__)
+
+with open("questions.json", encoding="utf-8") as f:
+    questions = json.load(f)
+
+@app.route("/", methods=["GET", "POST"])
+def questionnaire():
+    if request.method == "POST":
+        answers = dict(request.form)
+        return answers
+
+    return render_template("index.html", questions=questions)
+
+if __name__ == "__main__":
+    app.run(debug=True)
