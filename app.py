227c99cd1a6effa7dafae94ad0878f49c900a057
Marine <marinedoineau@gmail.com>
2026-02-11 22:30:53 +0100

    new questionnaire

--- app.py
+++ app.py
@@ -27,4 +27,4 @@ def result():
     return render_template("result.html", answers=answers)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    app.run(host="0.0.0.0", port=10000, debug=True)
